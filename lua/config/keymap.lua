d34df67f52e067645be8d31149829a79d58ce64c
Nicholas Jorgenson <nicholas.jorgenson@protonmail.com>
2025-02-03 14:39:09 -0500

    task: update keymap to allow easier text navigation when text is wrapped

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -5,6 +5,10 @@ local opts = { noremap = true, silent = true }
 vk.set('n', 'n', 'nzzzv', opts)          -- Next item in search is centered, folds are opened
 vk.set('n', 'N', 'Nzzzv', opts)          -- Previous item in search is centered, folds are opened
 vk.set('n', 'U', '<C-r>', opts)          -- Undo
+vk.set('n', 'j', 'gj', opts)             -- move cursor down on a wrapped line in normal mode
+vk.set('n', 'k', 'gk', opts)             -- move cursor up on a wrapped line in normal mode
+vk.set('v', 'j', 'gj', opts)             -- move cursor down on a wrapped line in visual mode
+vk.set('v', 'k', 'gk', opts)             -- move cursor up on a wrapped line in visual mode
 
 -- vk.set('v', '<D-x>', '"+x', opts) -- Cut
 -- vk.set('v', '<D-c>', '"+y', opts) -- Copy
